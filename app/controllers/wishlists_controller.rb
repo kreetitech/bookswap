23f2881e45a399ab3d9fe4dc70e61a73b43c7dd6
arijit chakraborty <arijit629@gmail.com>
2011-07-12 11:00:31 +0530

    wishlist

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,10 +1,17 @@
 class WishlistsController < ApplicationController
   def index
+     @wishlists=Wishlist.all
   end
 
   def create
+     @user =User.first 
+    @user.books.wishlists.create(params[:wishlist])
   end
 
+  def show
+ @title = @books.title
+  end
+  
   def destroy
   end
 
