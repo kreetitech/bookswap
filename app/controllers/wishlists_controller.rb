67af3a1c7831462c044d5067cb8f7202f7214a31
soumya <soumya@kreeti-desktop.(none)>
2011-07-13 13:21:19 +0530

    wishlist

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -4,12 +4,12 @@ class WishlistsController < ApplicationController
   end
 
   def create
-   current_user.inventories.create(:book_id => params[:book_id])
+   current_user.wishlists.create(:book_id => params[:book_id])
    redirect_to books_path
   end
 
   def show
- @title = @books.title
+  @wishlists=current_user.wishlists
   end
   
   def destroy
