72ed0b24e4a8a3348fb6711bb31c2e035d4ea896
arijit chakraborty <arijit629@gmail.com>
2011-07-14 08:01:14 +0530

    email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ActionMailer::Base
+  default :from => "notifications@example.com"
+ 
+  def welcome_email(user)
+    @user = user
+    @url  = "http://example.com/login"
+    mail(:to => user.email,
+         :subject => "Welcome to My Awesome Site")
+  end
+ 
+end
