6267eedc292d8378523803f91cb7d8801cff52c0
soumya manna <soumya@soumya-studio15.(none)>
2011-07-14 17:11:49 +0530

    homepage-Snehasis

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   skip_before_filter :require_user
 
   def index
+    @books = Book.all(:limit => 3, :conditions => ["photo_file_name is not NULL"], :order => "created_at desc")
+    @users = User.all(:limit => 10, :conditions => ["photo_file_name is not NULL"], :order => "created_at desc")
   end
 
 end
