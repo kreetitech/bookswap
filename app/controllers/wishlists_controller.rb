0b34cf8491cc112d88f6646d34b23276b17c0f02
Alapan Konar <alapan.konar@gmail.com>
2011-07-15 14:42:44 +0530

    Snehasis-Alapan

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -4,8 +4,8 @@ class WishlistsController < ApplicationController
   end
 
   def create
-   current_user.wishlists.create(:book_id => params[:book_id])
-   redirect_to books_path
+    current_user.wishlists.create(:book_id => params[:book_id])
+    redirect_to books_path
   end
 
   def show
